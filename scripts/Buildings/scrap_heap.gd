24c17bacac7e1197950838a9e42c682fd66b1c0b
HallurKrist <halh@itu.dk>
2023-11-13 13:48:33 +0100

    interaction event fixed

--- scripts/Buildings/scrap_heap.gd
+++ scripts/Buildings/scrap_heap.gd
@@ -1,18 +1,31 @@
 extends StaticBody2D
 
 # Is a player situated close to the scrap heap?
-var scrap_collectable := false
+var scrap_collectable_p1 := false
+var scrap_collectable_p2 := false
 
 func _process(_delta):
 	# Currently, the interaction button is F
-	if Input.is_action_just_pressed("interaction") and scrap_collectable:
+	if Input.is_action_just_pressed("interaction_p1") and scrap_collectable_p1:
+		ResourceManager.increase_scraps(25)
+		queue_free()
+		
+	if Input.is_action_just_pressed("interaction_p2") and scrap_collectable_p2:
 		ResourceManager.increase_scraps(25)
 		queue_free()
 
 func _on_player_detector_body_entered(body):
 	if body.is_in_group("players"):
-		scrap_collectable = true
+		if body.name == "Player":
+			scrap_collectable_p1 = true
+			
+		if body.name == "Player2":
+			scrap_collectable_p2 = true
 
 func _on_player_detector_body_exited(body):
 	if body.is_in_group("players"):
-		scrap_collectable = false
+		if body.name == "Player":
+			scrap_collectable_p1 = false
+			
+		if body.name == "Player2":
+			scrap_collectable_p2 = false
