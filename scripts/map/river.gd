e1c4512ddc6185beddf007d771e04335998400c5
HallurKrist <halh@itu.dk>
2023-11-13 14:22:46 +0100

    interactions fixed

--- scripts/map/river.gd
+++ scripts/map/river.gd
@@ -1,17 +1,27 @@
 extends StaticBody2D
 
 # Is a player situated close to the river?
-var water_collectable := false
+var water_collectable_p1 := false
+var water_collectable_p2 := false
 
 func _process(_delta):
-	# Currently, the interaction button is F
-	if Input.is_action_just_pressed("interaction") and water_collectable:
+	# Currently, the interaction button is F for p1 and action_down joystick for player2
+	if Input.is_action_just_pressed("interaction_p1") and water_collectable_p1:
+		ResourceManager.increase_water(25)
+		
+	if Input.is_action_just_pressed("interaction_p2") and water_collectable_p2:
 		ResourceManager.increase_water(25)
 
 func _on_player_detection_body_entered(body):
-	if body.is_in_group("players"):
-		water_collectable = true
+	if body.name == "Player":
+			water_collectable_p1 = true
+			
+	if body.name == "Player2":
+		water_collectable_p2 = true
 
 func _on_player_detection_body_exited(body):
-	if body.is_in_group("players"):
-		water_collectable = false
+	if body.name == "Player":
+		water_collectable_p1 = false
+			
+	if body.name == "Player2":
+		water_collectable_p2 = false
