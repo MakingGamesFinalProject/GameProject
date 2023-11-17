13d64cf8577e30763e9cc64b97f2916a08a8699e
Nick <nickvanzo437@gmail.com>
2023-11-17 15:38:18 +0100

    Added scrap sound effect

--- scripts/resource/resource_manager.gd
+++ scripts/resource/resource_manager.gd
@@ -4,22 +4,19 @@ extends Node
 var water : int = 0
 var energy : int = 0
 var scraps : int = 0
-var can_play_sound = true
+var can_play_water_sound = true
+var can_play_scrap_sound = true
 @export var time_to_wait_to_play_next_sound_in_seconds = 2
 
 func increase_water(amount):
-	if !$WaterPickUpSound.is_playing():
-		$WaterPickUpSound.play()
-		var time_manager = WaitUtil.new()
-		can_play_sound = false
-		time_manager.wait(time_to_wait_to_play_next_sound_in_seconds, self, "_reset_can_play_sound_after_timeout")
-		
+	play_water_pickup_sound()
 	water = clamp(water + amount, 0, 999)
 
 func increase_energy(amount):
 	energy = clamp(energy + amount, 0, 999)
 
 func increase_scraps(amount):
+	play_scrap_pickup_sound()
 	scraps = clamp(scraps + amount, 0, 999)
 
 func decrease_water(amount):
@@ -36,5 +33,22 @@ func reset_resources():
 	energy = 0
 	scraps = 0
 
-func _reset_can_play_sound_after_timeout():
-	can_play_sound = true
+func play_water_pickup_sound():
+	if !$WaterPickUpSound.is_playing():
+		$WaterPickUpSound.play()
+		var time_manager = WaitUtil.new()
+		can_play_water_sound = false
+		time_manager.wait(time_to_wait_to_play_next_sound_in_seconds, self, "_reset_can_play_sound_after_timeout_water")
+
+func play_scrap_pickup_sound():
+	if !$ScrapsPickUpSound.is_playing():
+		$ScrapsPickUpSound.play()
+		var time_manager = WaitUtil.new()
+		can_play_scrap_sound = false
+		time_manager.wait(time_to_wait_to_play_next_sound_in_seconds, self, "_reset_can_play_sound_after_timeout_scrap")
+
+func _reset_can_play_sound_after_timeout_water():
+	can_play_water_sound = true
+
+func _reset_can_play_sound_after_timeout_scrap():
+	can_play_scrap_sound = true
