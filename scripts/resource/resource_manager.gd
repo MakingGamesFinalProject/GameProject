566b592c1071ccb5504debc3cd627e8f43698e53
Nick <nickvanzo437@gmail.com>
2023-11-17 15:33:29 +0100

    Added sound effect when player picks up water

--- scripts/resource/resource_manager.gd
+++ scripts/resource/resource_manager.gd
@@ -4,8 +4,16 @@ extends Node
 var water : int = 0
 var energy : int = 0
 var scraps : int = 0
+var can_play_sound = true
+@export var time_to_wait_to_play_next_sound_in_seconds = 2
 
 func increase_water(amount):
+	if !$WaterPickUpSound.is_playing():
+		$WaterPickUpSound.play()
+		var time_manager = WaitUtil.new()
+		can_play_sound = false
+		time_manager.wait(time_to_wait_to_play_next_sound_in_seconds, self, "_reset_can_play_sound_after_timeout")
+		
 	water = clamp(water + amount, 0, 999)
 
 func increase_energy(amount):
@@ -27,3 +35,6 @@ func reset_resources():
 	water = 0
 	energy = 0
 	scraps = 0
+
+func _reset_can_play_sound_after_timeout():
+	can_play_sound = true
