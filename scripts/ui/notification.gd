0866ca650cb0e38af65530a3dd32d255fb0fd650
HallurKrist <halh@itu.dk>
2023-11-19 13:33:15 +0100

    notifications ready

--- scripts/ui/notification.gd
+++ scripts/ui/notification.gd
@@ -7,15 +7,19 @@ var notification_path = "res://data/notification.json"
 # The loaded JSON file
 var notifications := []
 
-#displaying dialog variables
+# displaying dialog variables
 var visible = false
 var current_id = -1
 
+# headshot folder path
+var headshot_path = "res://art/UI/headshots/"
+
 # notification fields
+@onready var notification : TextureRect = $dialog_popup
 @onready var headshot : TextureRect = $dialog_popup/HBoxContainer/Control/headshot
 @onready var notification_name : Label = $dialog_popup/HBoxContainer/Control2/VBoxContainer/Control/name
-@onready var title : Label = $dialog_popup/HBoxContainer/Control2/VBoxContainer/Control2/title
-@onready var dialog : Label = $dialog_popup/HBoxContainer/Control2/VBoxContainer/Control3/dialog
+@onready var notification_title : Label = $dialog_popup/HBoxContainer/Control2/VBoxContainer/Control2/title
+@onready var notification_text : Label = $dialog_popup/HBoxContainer/Control2/VBoxContainer/Control3/dialog
 
 func _ready():	
 	# Get JSON data and save in a variable
@@ -35,18 +39,31 @@ func _ready():
 	else:
 		print("JSON Parse Error: ", json.get_error_message(), " in ", json_string, " at line ", json.get_error_line())
 
+# Call once to show call again to hide
 func show_dialog(dialog_id: int):
-	
 	# if the state of the notification is visible then make it invisible and reset the current_id variable
 	if visible == true:
 		visible = false
 		current_id = -1
 		return # exit the function
 	
-	
 	# Put in correct info in the notification
-	
-	
-	
 	# make the notification visible
-	pass
+	for dialog in notifications:
+		if int ( dialog["id"] ) == dialog_id:
+			# load the headshot
+			var loaded_texture = load(headshot_path + dialog["picture_file"])
+			headshot.texture = loaded_texture
+			
+			# insert text
+			notification_name.text = dialog["name"]
+			notification_title.text = dialog["title"]
+			notification_text.text = dialog["text"]
+			
+			# set variables
+			visible = true
+			notification.visible = visible
+			current_id = int( dialog_id )
+	
+	
+	
