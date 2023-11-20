c2f9858412a3441ebee680dbe6d3e8dc192bf514
Steffen Jørgensen <stejo@itu.dk>
2023-11-20 09:07:14 +0100

    Pulse effect, fade and sounds

--- scripts/Buildings/building_plot.gd
+++ scripts/Buildings/building_plot.gd
@@ -1,5 +1,13 @@
 extends StaticBody2D
 
+# Time it takes for the silhoutte to pulse in/out
+const SILHOUETTE_PULSE_TIME : float = 1.0
+# Time it takes for the building to fade in (currently matches the building sound)
+const BUILDING_FADE_TIME: float = 10.52
+
+# Keep track of the tween state
+var silhouette_is_pulsing := false
+
 var player1_is_close := false
 var player2_is_close := false
 var can_be_build := false
@@ -14,12 +22,19 @@ func _process(_delta):
 		$Collider.disabled = false
 		$SimpleHouseSilhouette.show()
 
+		# Make the silhouette pulse if it's not
+		if not silhouette_is_pulsing:
+			silhouette_is_pulsing = true
+			silhouette_pulse()
+
 	if Input.is_action_just_pressed("interaction_p1") and can_be_build and player1_is_close:
 		ResourceManager.decrease_water(50)
 		ResourceManager.decrease_scraps(50)
 
 		$SimpleHouseSilhouette.queue_free()
 		$SimpleHouse.show()
+		building_fade_in()
+		building_play_sounds()
 		
 	if Input.is_action_just_pressed("interaction_p2") and can_be_build and player2_is_close:
 		ResourceManager.decrease_water(50)
@@ -27,6 +42,28 @@ func _process(_delta):
 
 		$SimpleHouseSilhouette.queue_free()
 		$SimpleHouse.show()
+		building_fade_in()
+		building_play_sounds()
+
+# The function simply modifies the alpha value of the sprite
+func silhouette_pulse():
+	var fade = create_tween()
+	fade.tween_property($SimpleHouseSilhouette, "modulate", Color(1, 1, 1, 0), SILHOUETTE_PULSE_TIME). \
+		from(Color(1, 1, 1, 1))
+	fade.tween_property($SimpleHouseSilhouette, "modulate", Color(1, 1, 1, 1), SILHOUETTE_PULSE_TIME). \
+		from(Color(1, 1, 1, 0))
+	await(fade.finished)
+	silhouette_is_pulsing = false
+
+func building_fade_in():
+	var fade = create_tween()
+	fade.tween_property($SimpleHouse, "modulate", Color(1, 1, 1, 1), BUILDING_FADE_TIME). \
+		from(Color(1, 1, 1, 0))
+
+func building_play_sounds():
+	$Building.play()
+	await($Building.finished)
+	$Built.play()
 
 func sufficient_resources():
 	if ResourceManager.water >= 50 and ResourceManager.scraps >= 50:
