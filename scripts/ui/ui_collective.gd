1066b8f6351232dafe2ead2cbf9f7ea0ffeab43c
Nick <nickvanzo437@gmail.com>
2023-12-03 14:56:05 +0100

    Animations for resource gathering finished

--- scripts/ui/ui_collective.gd
+++ scripts/ui/ui_collective.gd
@@ -7,7 +7,7 @@ extends CanvasLayer
 @onready var water_animation_counter_timer: Timer = $MarginContainer/Control/WaterResourceCounter/WaterAnimationTimer
 @onready var energy_animation_counter_timer: Timer = $MarginContainer/Control/EnergyResourceCounter/EnergyAnimationTimer
 @onready var scraps_animation_counter_timer: Timer = $MarginContainer/Control/ScrapsResourceCounter/ScrapsAnimationTimer
-
+@export var delay_to_increase_resource_during_animation_in_seconds = 0.02
 func _ready():
 	ResourceManager.on_resource_increased.connect(animate_resource_incrase)
 	water_animation_counter_timer.wait_time = $MarginContainer/Control/WaterResourceCounter/WaterTexture2/AnimationPlayer.get_animation("resource_up").length - 0.2
@@ -33,21 +33,35 @@ func animate_resource_incrase(resource, amount_increased):
 		water_animation_counter_timer.start()
 		var current_water_resource = int(water_label.text)
 		var new_value = current_water_resource + amount_increased
-		water_label.text = str(new_value)
+		
+		while(current_water_resource < new_value):
+			current_water_resource += 1
+			water_label.text = str(current_water_resource)
+			await get_tree().create_timer(delay_to_increase_resource_during_animation_in_seconds).timeout
 	elif resource == "energy":
 		$MarginContainer/Control/EnergyResourceCounter/EnergyTexture2/AnimationPlayer.play("resource_up")
 		energy_animation_counter_timer.start()
 		var current_resource = int(energy_label.text)
 		var new_value = current_resource + amount_increased
-		energy_label.text = str(new_value)
+		
+		while(current_resource < new_value):
+			current_resource += 1
+			energy_label.text = str(current_resource)
+			await get_tree().create_timer(delay_to_increase_resource_during_animation_in_seconds).timeout
+		
 	elif resource == "scraps":
 		$MarginContainer/Control/ScrapsResourceCounter/ScrapsTexture2/AnimationPlayer.play("resource_up")
 		scraps_animation_counter_timer.start()
 		var current_resource = int(scraps_label.text)
 		var new_value = current_resource + amount_increased
-		scraps_label.text = str(new_value)
+		
+		while(current_resource < new_value):
+			current_resource += 1
+			scraps_label.text = str(current_resource)
+			await get_tree().create_timer(delay_to_increase_resource_during_animation_in_seconds).timeout
 	else:
 		assert(false, "Resource not valid, please pass one of the following strings [water, scraps, energy]")
+
 
 func animate_resource_block_water():
 	$MarginContainer/Control/WaterResourceCounter/AnimationPlayer.play("resource_counter_up")
