2236ac697e525ab647a52bcf8efae39f226381ed
HallurKrist <halh@itu.dk>
2023-12-06 14:58:36 +0100

    some fixes

--- scripts/Buildings/wind_turbine.gd
+++ scripts/Buildings/wind_turbine.gd
@@ -4,11 +4,9 @@ enum available_states {WORKING, TO_REPAIR, REPAIRING}
 
 var counter_players_detected = 0
 var task_manager_ref = null
-var house_ref = null
 var current_status = available_states.WORKING
 
 @export var time_to_repair_in_seconds = 5
-@export var time_to_complete_check_batteries_task_in_seconds = 3
 
 var detected_foliage : Array[Node2D] = []
 
@@ -29,9 +27,19 @@ var has_been_built := false
 var is_collectable := false
 var COLLECTABILITY_TIMER: float = 20.0
 
+#############################################################################################
+############################DIFFERENT FOR EVERY BUILDING ####################################
+#####################################vvvvv###################################################
+var needed_for_build_water := 5
+var needed_for_build_energy := 0
+var needed_for_build_scrap := 0
+
 var collection_resource_water := 5
 var collection_resource_energy := 0
 var collection_resource_scrap := 5
+#########################################^^^^################################################
+############################DIFFERENT FOR EVERY BUILDING ####################################
+#############################################################################################
 
 func detect_foliage(area):
 	if area.get_parent().is_in_group("Grass") or area.get_parent().is_in_group("Bush") or \
@@ -40,7 +48,6 @@ func detect_foliage(area):
 
 func _ready():
 	set_task_manager_ref()
-	set_house_ref()
 	$Collider.hide()
 	$Base.hide()
 	$Base/Helix.hide()
@@ -64,8 +71,9 @@ func _process(_delta):
 
 	if Input.is_action_just_pressed("interaction_p1") and  player1_is_close:
 		if can_be_build and not has_been_built:
-			ResourceManager.decrease_water(50)
-			ResourceManager.decrease_scraps(50)
+			ResourceManager.decrease_water(needed_for_build_water)
+			ResourceManager.decrease_energy(needed_for_build_energy)
+			ResourceManager.decrease_scraps(needed_for_build_scrap)
 			has_been_built = true
 
 			$Outline.queue_free()
@@ -79,10 +87,11 @@ func _process(_delta):
 			give_resources()
 		
 	if Input.is_action_just_pressed("interaction_p2") and player2_is_close:
-		
 		if can_be_build and not has_been_built:
-			ResourceManager.decrease_water(50)
-			ResourceManager.decrease_scraps(50)
+			ResourceManager.decrease_water(needed_for_build_water)
+			ResourceManager.decrease_energy(needed_for_build_energy)
+			ResourceManager.decrease_scraps(needed_for_build_scrap)
+			has_been_built = true
 
 			$Outline.queue_free()
 			$Base.show()
@@ -93,9 +102,11 @@ func _process(_delta):
 		elif has_been_built and is_collectable:
 			start_collection_timer()
 			give_resources()
-	
+
 func sufficient_resources():
-	if ResourceManager.scraps >= 5 and ResourceManager.energy >= 10:
+	if ResourceManager.water >= needed_for_build_water \
+	and ResourceManager.energy >= needed_for_build_energy \
+	and ResourceManager.scraps >= needed_for_build_scrap:
 		return true
 	else:
 		return false
@@ -136,48 +147,63 @@ func building_play_sounds():
 	$Built.play()
 	has_been_built = true
 	start_collection_timer()
+	
+#############################################################################################
+############################DIFFERENT FOR EVERY BUILDING ####################################
+#####################################vvvvv###################################################
 
 func check_task_completion():
+	# This function needs to be here but it is supposed to 
+	# call the correct "check tasks" depending on the building
 	check_for_fixing_task()
 	check_for_batteries_task()
 
 func check_for_batteries_task():
-	if current_status != available_states.WORKING:
+	if current_status != available_states.WORKING: 
 		return
 	
 	# Check if both players have the check batteries task assigned
-	if task_manager_ref.get_current_task(1) == 3 and task_manager_ref.get_current_task(2) == 3:
-		var a_user_is_interacting = Input.is_action_pressed("interaction_p1") || Input.is_action_pressed("interaction_p2")
-		var amount_players_interacting_with_house = house_ref.players_counter_on_area
-		if a_user_is_interacting and counter_players_detected > 0 and amount_players_interacting_with_house > 0:
-			var time_manager = WaitUtil.new()
-			time_manager.wait(time_to_repair_in_seconds, self, "_on_battery_task_callback")
+	var task_id = task_manager_ref.get_task_by_name("Check batteries (Wind Turbines)").uid
+	if task_manager_ref.get_current_task(task_id) == 3 and task_manager_ref.get_current_task(task_id) == 3:
+		# a player is interacting with the wind turbine
+		if (Input.is_action_pressed("interaction_p1") and player1_is_close) \
+		 or (Input.is_action_pressed("interaction_p2") and player2_is_close):
+			var house_ref = get_tree().get_first_node_in_group("house")
+			assert(house_ref != null, "House reference not found")
+			# a player interacting with the house and the wind turbine
+			var amount_players_interacting_with_house = house_ref.players_counter_on_area
+			if counter_players_detected > 0 and amount_players_interacting_with_house > 0:
+				var time_manager = WaitUtil.new()
+				time_manager.wait(time_to_repair_in_seconds, self, "_on_battery_task_callback")
 
 func _on_battery_task_callback():
 	task_manager_ref.set_task_as_done(3)
 
 func check_for_fixing_task():
-	var a_user_is_interacting = Input.is_action_pressed("interaction_p1") || Input.is_action_pressed("interaction_p2")
+	if current_status == available_states.TO_REPAIR: # check if the building needs repair
+		return
+		
+	var a_user_is_interacting = Input.is_action_pressed("interaction_p1") or Input.is_action_pressed("interaction_p2")
 	if counter_players_detected > 0:
-		if a_user_is_interacting && current_status == available_states.TO_REPAIR:
+		if a_user_is_interacting and (current_status == available_states.TO_REPAIR):
 			repair_self()
 
 func repair_self():
 	current_status = available_states.REPAIRING
 	var time_manager = WaitUtil.new()
 	time_manager.wait(time_to_repair_in_seconds, self, "_on_repair_complete_callback")
-	
+
 func _on_repair_complete_callback():
 	current_status = available_states.WORKING
-	task_manager_ref.set_task_as_done(2)
+	task_manager_ref.set_task_as_done(2) # NEEDS THE CORRECT TASK ID FOR THIS BUILDING
+
+#######################################^^^^##################################################
+############################DIFFERENT FOR EVERY BUILDING ####################################
+#############################################################################################
 
 func set_task_manager_ref():
 	task_manager_ref = get_tree().get_first_node_in_group("task_manager")
 	assert(task_manager_ref != null, "Task manager not found")
-
-func set_house_ref():
-	house_ref = get_tree().get_first_node_in_group("house")
-	assert(house_ref != null, "House reference not found")
 
 func _on_detection_area_body_entered(body):
 	if body.is_in_group("players"):
