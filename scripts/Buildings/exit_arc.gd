728d356924c29d0e0a7248d0cf066bdd69b74780
HallurKrist <halh@itu.dk>
2023-12-09 15:30:38 +0100

    arc works for switching maps

--- scripts/Buildings/exit_arc.gd
+++ scripts/Buildings/exit_arc.gd
@@ -1,11 +1,12 @@
 extends StaticBody2D
 
-var counter_players_detected = 0
+var player1_is_close = false
+var player2_is_close = false
+
 var task_manager_ref = null
+var npc_ref = null
 
 var detected_foliage : Array[Node2D] = []
-
-@onready var world_script = "../levels/world.gd"
 
 func detect_foliage(area):
 	if area.get_parent().is_in_group("Grass") or area.get_parent().is_in_group("Bush") or \
@@ -14,15 +15,21 @@ func detect_foliage(area):
 
 func _ready():
 	set_task_manager_ref()
+	set_npc_ref()
+	#var world = new world_script()
 
 func _process(_delta):	
-	if counter_players_detected == 2 and check_task_completion():
+	# and check_task_completion()
+	if player1_is_close and player2_is_close:
 		if check_condiditons():
 			# end game
 			pass
 		else:
 			#next map
-			world_script.regenerate()
+			var world = get_tree().get_first_node_in_group("World")
+			assert(world, "World not found")
+			reset_players()
+			world.regenerate()
 	
 func check_condiditons():
 	if NPCEncounter.huggy == 3 \
@@ -33,22 +40,40 @@ func check_condiditons():
 	return false
 		
 func check_task_completion():
-	var npc_ref = get_tree().get_first_node_in_group("npc")
 	var task_ids = npc_ref.npc_task_ids
 	for id in task_ids:
-		if not task_manager_ref.is_task_completed():
+		if not task_manager_ref.is_task_completed(id):
 			return false
 	return true
 
 func set_task_manager_ref():
 	task_manager_ref = get_tree().get_first_node_in_group("task_manager")
 	assert(task_manager_ref != null, "Task manager not found")
+	
+func set_npc_ref():
+	npc_ref = get_tree().get_first_node_in_group("npc")
+	assert(npc_ref != null, "NPC not found")
+	
+func reset_players():
+	var player_array = get_tree().get_nodes_in_group("players")
+	player_array[0].global_position.x = 300
+	player_array[0].global_position.y = 2400
+	player_array[1].global_position.x = 300
+	player_array[1].global_position.y = 2600
 
 func _on_detection_area_body_entered(body):
 	if body.is_in_group("players"):
-		++counter_players_detected;
+		if body.name == "Player":
+			player1_is_close = true
+			
+		if body.name == "Player2":
+			player2_is_close = true
 
 func _on_detection_area_body_exited(body):
 	if body.is_in_group("players"):
-		--counter_players_detected;
+		if body.name == "Player":
+			player1_is_close = false
+			
+		if body.name == "Player2":
+			player2_is_close = false
 
